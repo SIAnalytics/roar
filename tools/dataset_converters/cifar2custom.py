0f7be308fa021b2fe42fd8a188d44d51d3eb66cf
KKIEEK <kkieek@KKIEEKui-MacBookPro.local>
2023-01-14 15:25:34 +0900

    Add dataset converters

---
+++ tools/dataset_converters/cifar2custom.py
@@ -0,0 +1,37 @@
+import argparse
+import os.path as osp
+from tempfile import TemporaryDirectory
+
+import mmcv
+
+from mmcls.datasets import CIFAR10
+
+
+def parse_args():
+    parser = argparse.ArgumentParser(
+        description='Convert CIFAR to CustomDataset format')
+    parser.add_argument('-o', '--out-dir', required=True, help='output path')
+    args = parser.parse_args()
+    return args
+
+
+def main():
+    args = parse_args()
+
+    tmpdir = TemporaryDirectory()
+    cifar = CIFAR10(tmpdir.name, test_mode=False)
+    for data in cifar:
+        base_dir = osp.join(args.out_dir, 'train', str(data['gt_label']))
+        mmcv.imwrite(data['img'],
+                     osp.join(base_dir, data['sample_idx'] + '.png'))
+
+    cifar = CIFAR10(tmpdir.name, test_mode=True)
+    for data in cifar:
+        base_dir = osp.join(args.out_dir, 'test', str(data['gt_label']))
+        mmcv.imwrite(data['img'],
+                     osp.join(base_dir, data['sample_idx'] + '.png'))
+    tmpdir.cleanup()
+
+
+if __name__ == '__main__':
+    main()
