87501280af89152ae1541bb9c43bb2224d5cd6d7
KKIEEK <kkieek@KKIEEKui-MacBookPro.local>
2023-01-14 14:26:14 +0900

    Fix

--- roar/datasets/svhn.py
+++ roar/datasets/svhn.py
@@ -1,14 +1,19 @@
+from typing import Optional
+from urllib.parse import urljoin
+
 import mmengine.dist as dist
 import numpy as np
-from mmengine.fileio import LocalBackend, get_file_backend, join_path
+from mmengine.fileio import LocalBackend, exists, get_file_backend, join_path
 from scipy.io import matlab
 
-from mmcls.datasets import MNIST
+from mmcls.datasets import BaseDataset
+from mmcls.datasets.categories import MNIST_CATEGORITES
+from mmcls.datasets.utils import download_url
 from mmcls.registry import DATASETS
 
 
 @DATASETS.register_module()
-class SVHN(MNIST):
+class SVHN(BaseDataset):
     """`SVHN <http://ufldl.stanford.edu/housenumbers>`_ Dataset.
 
     Args:
@@ -34,14 +39,34 @@ class SVHN(MNIST):
         ['test_32x32.mat', 'eb5a983be6a315427106f1b164d9cef3'],
     ]
     # extra images and labels, but it is not used
-    extra_list = [['extra_32x32.mat', 'a93ce644f1a588dc4d68dda5feec44a7']]
+    extra_list = [
+        ['extra_32x32.mat', 'a93ce644f1a588dc4d68dda5feec44a7'],
+    ]
+    METAINFO = {'classes': MNIST_CATEGORITES}
+
+    def __init__(self,
+                 data_prefix: str,
+                 test_mode: bool,
+                 metainfo: Optional[dict] = None,
+                 data_root: str = '',
+                 download: bool = True,
+                 **kwargs):
+        self.download = download
+        super().__init__(
+            # The SVHN dataset doesn't need specify annotation file
+            ann_file='',
+            metainfo=metainfo,
+            data_root=data_root,
+            data_prefix=dict(root=data_prefix),
+            test_mode=test_mode,
+            **kwargs)
 
     def load_data_list(self):
         """Load images and ground truth labels."""
         root = self.data_prefix['root']
         backend = get_file_backend(root, enable_singleton=True)
 
-        if dist.is_main_process():
+        if dist.is_main_process() and not self._check_exists():
             if not isinstance(backend, LocalBackend):
                 raise RuntimeError(f'The dataset on {root} is not integrated, '
                                    f'please manually handle it.')
@@ -66,12 +91,30 @@ class SVHN(MNIST):
 
         # load data from mat file
         mat = matlab.loadmat(join_path(root, file_list[0][0]))
-        imgs = mat['X'].transpose(3, 0, 1, 2)
-        gt_labels = mat['y']
+        imgs = np.transpose(mat['X'], (3, 0, 1, 2))  # convert HWCN to NHWC
+        gt_labels = np.squeeze(mat['y'])  # convert N1 to N
 
         data_infos = []
         for img, gt_label in zip(imgs, gt_labels):
-            gt_label = np.array(gt_label, dtype=np.int8)
-            info = {'img': img.numpy(), 'gt_label': gt_label}
+            info = dict(img=img, gt_label=gt_label)
             data_infos.append(info)
         return data_infos
+
+    def _check_exists(self):
+        """Check the exists of data files."""
+        root = self.data_prefix['root']
+
+        for filename, _ in (self.train_list + self.test_list):
+            # get extracted filename of data
+            fpath = join_path(root, filename)
+            if not exists(fpath):
+                return False
+        return True
+
+    def _download(self):
+        """Download and extract data files."""
+        root = self.data_prefix['root']
+
+        for filename, md5 in (self.train_list + self.test_list):
+            url = urljoin(self.url_prefix, filename)
+            download_url(url, root, filename=filename, md5=md5)
